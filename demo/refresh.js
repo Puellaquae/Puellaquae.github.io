1841e6dfb07fe394fc8360f6c557048808f6c3c6
Puellaquae <shentukeqin@hotmail.com>
2021-02-22 16:38:57 +0800

    使用 GIST 保存 Token

--- demo/refresh.js
+++ demo/refresh.js
@@ -1,46 +1,53 @@
-var fs = require('fs')
-var fetch = require('node-fetch');
-
 function abort() {
      process.emit("exit")
      process.reallyExit(10)
 }
 
-fs.readFile('demo/token.js', (err, data) => {
-     if (err) {
-          console.error(err)
-          abort()
-     } else {
-          let t = data.toString()
-          eval(t)
-          console.log('got old token')
-          console.log(refreshToken)
-          fetch('https://login.microsoftonline.com/common/oauth2/v2.0/token', {
-                    headers: {
-                         'Content-Type': 'application/x-www-form-urlencoded',
-                         'Origin':'http://localhost/index.html'
-                    },
-                    body: 'client_id=' + clientId + '&scope=offline_access Files.ReadWrite&refresh_token=' + refreshToken + '&grant_type=refresh_token',
-                    method: 'POST'
-               })
-               .then(r => r.json())
-               .then(res => {
-                    refreshToken = res.refresh_token
-                    if (refreshToken == null || refreshToken == undefined) {
-                         console.log(JSON.stringify(res, null, 2))
-                         abort()
-                    } else {
-                         console.log('got new token')
-                         console.log(refreshToken)
-                         fs.writeFile('demo/token.js', 'var clientId = "' + clientId + '"\nvar refreshToken = "' + refreshToken + '"', e => {
-                              if (e) {
-                                   console.log(e)
-                                   abort()
-                              } else {
-                                   console.log('saved!')
-                              }
-                         })
-                    }
-               })
-     }
-})
+const {
+     Octokit
+ } = require("@octokit/core")
+ 
+ var fetch = require('node-fetch');
+ 
+ const octokit = new Octokit({
+     auth: process.env.GistAuth
+ })
+ 
+ var clientId = "58c536e5-d57e-4160-abc8-83a11e040807"
+ var gistId = 'c4ab586d153ecf56b386ac2db0728263'
+ 
+ 
+ octokit.request('GET /gists/{gist_id}', {
+     gist_id: gistId
+ }).then(r => r.data).then(r => {
+     let refreshToken = r.files["token.txt"].content
+     console.log('Got Old Token')
+     console.log(refreshToken)
+     fetch('https://login.microsoftonline.com/common/oauth2/v2.0/token', {
+             headers: {
+                 'Content-Type': 'application/x-www-form-urlencoded',
+                 'Origin': 'http://localhost/index.html'
+             },
+             body: 'client_id=' + clientId + '&scope=offline_access Files.ReadWrite&refresh_token=' + refreshToken + '&grant_type=refresh_token',
+             method: 'POST'
+         })
+         .then(r => r.json())
+         .then(res => {
+             refreshToken = res.refresh_token
+             if (refreshToken == null || refreshToken == undefined) {
+                 console.log(JSON.stringify(res, null, 2))
+                 abort()
+             } else {
+                 console.log('got new token')
+                 console.log(refreshToken)
+                 octokit.request('PATCH /gists/{gist_id}', {
+                     gist_id: gistId,
+                     files: {
+                         "token.txt": {
+                             'content': refreshToken
+                         }
+                     }
+                 })
+             }
+         })
+ })
